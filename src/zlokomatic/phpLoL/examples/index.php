19e9c8dc47a2545312d2c588651ce05b8f59dfe1
Emanuel Lujan <ema_luj@hotmail.com>
2013-10-19 15:21:27 -0300

    Update index.php

--- src/zlokomatic/phpLoL/examples/index.php
+++ src/zlokomatic/phpLoL/examples/index.php
@@ -4,6 +4,8 @@ require_once 'vendor/autoload.php';
 
 use zlokomatic\phpLoL\LoLClient;
 
+//use zlokomatic\phpLoL\examples\LOLProfiler;
+
 $client = new LoLClient('Username', 'Password', 'NA' );
 
 $summoner = $client->getSummonerByName("SummonerName");
@@ -18,3 +20,13 @@ $stats = $client->getPlayerStatsByAccountId($summoner->getAcctId());
 $aggStats = $client->getAggregatedStats($summoner->getAcctId(), 'CLASSIC', 'CURRENT');
 
 $leagues = $client->getAllLeaguesForPlayer($summoner->getSummonerId());
+
+/*
+
+$profiler = new LOLProfiler('Username','Password','NA');
+
+$profiler->searchSummoner('SummonerName');
+
+echo $profiler->name;
+
+*/
