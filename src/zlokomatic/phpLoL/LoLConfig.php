b678f2764e326e9a488a821581fe4a15715feb45
Emanuel Lujan <ema_luj@hotmail.com>
2013-10-18 23:54:17 -0300

    LAS and LAN added
    I've added LAS and LAN region ( latin south america ) in LOLConfig.php

--- src/zlokomatic/phpLoL/LoLConfig.php
+++ src/zlokomatic/phpLoL/LoLConfig.php
@@ -6,16 +6,20 @@ class LoLConfig
 {
     private $clientVersion = "3.12.13_10_01_11_40";
     private $region;
-    private $regions = array('EUW', 'EUNE', 'NA', 'BR');
+    private $regions = array('EUW', 'EUNE', 'NA', 'BR', 'LAN', 'LAS');
     private $login_queue_host = array('EUW' => 'lq.eu.lol.riotgames.com',
                                       'EUNE' => 'lq.eun1.lol.riotgames.com',
                                       'NA' => 'lq.na1.lol.riotgames.com',
                                       'BR' => 'lq.br.lol.riotgames.com',
+                                      'LAN' => 'lq.la1.lol.riotgames.com',
+                                      'LAS' => 'lq.la2.lol.riotgames.com',
                                      );
     private $rpc_host = array('EUW' => 'prod.eu.lol.riotgames.com',
                               'EUNE' => 'prod.eun1.lol.riotgames.com',
                               'NA' => 'prod.na1.lol.riotgames.com',
                               'BR' => 'prod.br.lol.riotgames.com',
+                              'LAN' => 'prod.la1.lol.riotgames.com',
+                              'LAS' => 'prod.la2.lol.riotgames.com',
                              );
 
     public function __construct($region)
