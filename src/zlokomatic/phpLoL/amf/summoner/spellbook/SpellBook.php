c97fed3e502e9bd35f135021bbb34a314a3ae4d6
Emanuel Lujan <ema_luj@hotmail.com>
2013-10-20 20:43:24 -0200

    Update SpellBook.php

--- src/zlokomatic/phpLoL/amf/summoner/spellbook/SpellBook.php
+++ src/zlokomatic/phpLoL/amf/summoner/spellbook/SpellBook.php
@@ -2,85 +2,10 @@
 
 namespace zlokomatic\phpLoL\amf\summoner\spellbook;
 
-use zlokomatic\phpLoL\amf\LoLAMFBase;
 
-class SpellBook extends LoLAMFBase
+use zlokomatic\phpLoL\amf\IBookTemplate;
+
+class SpellBook extends IBookTemplate
 {
-    protected $bookPagesJson;
-    protected $bookPages = array();
-    protected $dateString;
-    protected $summonerId;
-
-    function __construct()
-    {
-
-    }
-
-    /**
-     * @param mixed $bookPagesJson
-     */
-    public function setBookPagesJson($bookPagesJson)
-    {
-        $this->bookPagesJson = $bookPagesJson;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getBookPagesJson()
-    {
-        return $this->bookPagesJson;
-    }
-
-    /**
-     * @param mixed $bookPages
-     */
-    public function setBookPages($bookPages)
-    {
-        foreach($bookPages as $page){
-            $this->bookPages[] = $page;
-        }
-
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getBookPages()
-    {
-        return $this->bookPages;
-    }
-
-    /**
-     * @param mixed $dateString
-     */
-    public function setDateString($dateString)
-    {
-        $this->dateString = $dateString;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getDateString()
-    {
-        return $this->dateString;
-    }
-
-    /**
-     * @param mixed $summonerId
-     */
-    public function setSummonerId($summonerId)
-    {
-        $this->summonerId = $summonerId;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getSummonerId()
-    {
-        return $this->summonerId;
-    }
-
+    // EDIT IBookTemplate.php
 } 
