12930ce61276d786ad546467dc6b807a672cc460
Emanuel Lujan <ema_luj@hotmail.com>
2013-10-20 20:41:44 -0200

    Create MasteryBookDTO.php

---
+++ src/zlokomatic/phpLoL/amf/summoner/masterybook/MasteryBookDTO.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace zlokomatic\phpLoL\amf\summoner\masterybook;
+
+
+use zlokomatic\phpLoL\amf\IBookTemplate;
+
+class MasteryBookDTO extends IBookTemplate
+{
+    // EDIT IBookTemplate.php
+}
